160a6ae01e21d55aa9342b0034e8de748d75df4f
Oskar Niburski <oskarniburski@gmail.com>
2013-11-15 11:30:52 -0500

    Changed omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '660552000654821', '9af93ddb33c00a5a572d8ea6caf52473'
 end
 
-Omniauth.config.on_failure = Proc.new do |env|
+OmniAuth.config.on_failure = Proc.new do |env|
   SessionsController.action(:failure).call(env)
   #this will invoke the omniauth_failure action in UsersController.
 end
