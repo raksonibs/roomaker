af4c26ce810d0bb54d667453ce0269d1818bde7d
Oskar Niburski <oskarniburski@gmail.com>
2013-11-24 00:09:08 -0500

    Basic disaply improved, stats page added for each house. Want to work on a few notes, but want some javascript in. Jquery upgrade pronto

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,89 @@
 module GroupsHelper
+	def themost(group)
+		max=0
+		maxuser="bob"
+ 		group.users.each do |u| 
+     		usertotal=0 
+     		u.completedtasks.each do |ct|
+        		if ct.group == group.name
+           			usertotal+=1 if u.id==ct.completer_id
+        		end
+        	end
+        	if usertotal>=max
+        		max=usertotal 
+        		maxuser=u
+        	end
+		end
+		return maxuser
+
+	end
+
+	def theleast(group)
+		min=10000
+		minuser=nil
+ 		group.users.each do |u| 
+     		usertotal=0 
+     		u.completedtasks.each do |ct|
+        		if ct.group == group.name
+           			usertotal+=1 if u.id==ct.completer_id
+        		end
+        	end
+        	if usertotal<=min
+        		min=usertotal 
+        		minuser=u
+        	end
+		end
+		return minuser
+	end
+
+
+	def mostrecent(group)
+		min=0
+		minuser=nil
+		minid=-1
+ 		group.users.each do |u| 
+     		usertotal=0 
+     		u.completedtasks.each do |ct|
+        		if ct.group == group.name
+
+        			if min==0 || ct.created_at > min
+    
+        				min=ct.created_at
+        				minid=ct.id
+
+        				minuser=u
+
+        			end
+        		end
+        	end
+        	answer=[minuser,minid]
+
+        	return answer
+		end
+
+		
+	end
+
+	def leastrecent(group)
+		val=[]
+		group.users.each do |u|
+			val << u.completedtasks.where(group: group.name).first unless u.completedtasks.where(group: group.name).first==nil
+		end
+
+
+		answer=[val.min_by{ |x| x.id }.user_id, val.min_by{ |x| x.id }]
+		return answer
+	end
+
+	def never(group)
+		val={}
+		group.users.each do |u|
+			val[u]= u.completedtasks.where(group: group.name).first
+		end
+		
+		answer=val.select{|c| val[c]==nil}
+
+		
+		return answer
+	end
 end
