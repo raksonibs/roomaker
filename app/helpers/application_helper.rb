4b2bc187f2d0baa4d43a0760501e1cddf9a679ab
Oskar Niburski <oskarniburski@gmail.com>
2013-11-23 19:57:12 -0500

    Still working on damn display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def selectmax(pendingtasks,acceptedtasks,completedtasks,currenttasks,incompletetasks)
+      arr=[pendingtasks,acceptedtasks,completedtasks,currenttasks,incompletetasks]
+      return arr.max_by {|x| x.size }.size
+    end
 end
